4db79f97eee21b47db8fb8f1b633b8cfb5f44d12
killerkoderson-sudo <killerkoderson@gmail.com>
2025-11-11 15:50:18 +0000

    Update greeting.py

--- greeting.py
+++ greeting.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 def hello(): # Name of the method
  return("Hello World!")
 @app.route("/greetme")#different URL
+
+@app.route("/greetjohn")#URL leading to method
+def hello(): # Name of the method
+ return("Hello john!")
+
+
+@app.route("/greetme")#different URL
 def helloall(): # different method name
  name = request.args.get('name')#retrieve GET parameters
  return("Hello {}!".format(name))#Python’s string.format
+ 
 if __name__ == "__main__":
  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
